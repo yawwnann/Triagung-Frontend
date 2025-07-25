a61280f230f7e20727abc2dbd6f99d0c10b13fc0
yawwnann <c0478592@gmail.com>
2025-07-26 06:56:03 +0700

    Integrate AOS for animations in HomeView: Add AOS library and initialize it in useEffect for smooth scrolling effects. Update component structure to include data attributes for animation delays on HeroSection, ProductHighlight, StatsSection, and TestimonialSection. Update package.json and package-lock.json to include AOS and its types.

--- src/home/HomeView.tsx
+++ src/home/HomeView.tsx
@@ -1,5 +1,7 @@
 // HomeView.tsx
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 // Hapus import Navbar karena sudah di-render di App.tsx
 // import { Navbar } from "../common/components/Navbar";
 import HeroSection from "./components/HeroSection";
@@ -8,12 +10,27 @@ import StatsSection from "./components/StatsSection";
 import TestimonialSection from "./components/TestimonialSection";
 
 const HomeView: React.FC = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      once: true,
+    });
+  }, []);
+
   return (
     <div className="font-Montserrat">
-      <HeroSection />
-      <ProductHighlight />
-      <StatsSection />
-      <TestimonialSection />
+      <div data-aos="fade-up">
+        <HeroSection />
+      </div>
+      <div data-aos="fade-up" data-aos-delay="100">
+        <ProductHighlight />
+      </div>
+      <div data-aos="fade-up" data-aos-delay="200">
+        <StatsSection />
+      </div>
+      <div data-aos="fade-up" data-aos-delay="300">
+        <TestimonialSection />
+      </div>
     </div>
   );
 };
